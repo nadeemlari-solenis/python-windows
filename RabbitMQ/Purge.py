19a29310ea4b223986db1856c6cfd5c17e8fdcf1
Nadeem <mnadeem@contractor.solenis.com>
2023-11-29 19:09:04 +0530

    added

---
+++ RabbitMQ/Purge.py
@@ -0,0 +1,37 @@
+import pika, os, time
+
+# def process_message(msg):
+#   print("Message is \n")
+#   print(str(msg))
+#   time.sleep(0.1) # delays for 0.5 seconds
+  
+
+params = pika.URLParameters("amqp://solrmquser:HDznY99bPybeoe8d48ts@172.0.55.8:5672/")
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+allQs=["dlq-digi-measurements","dlq-realtiteq-alarms","dlq-realtiteq-measurements1","dlq-realtiteq-measurements2","dlq-realtiteq-measurements3","dlq-realtiteq-measurements4","dlq-realtiteq-measurements5","dlq-realtiteq-measurements6","dlq-realtiteq-measurements7","dlq-realtiteq-measurements8","dlq-realtiteq-measurements9","dlq-realtiteq-measurements10","dlq-realtiteq-measurements11","dlq-realtiteq-measurements12","dlq-realtiteq-measurements13","dlq-realtiteq-measurements14","dlq-realtiteq-measurements15","temp-bkup"]
+for q in allQs:
+    print("Purging queue "+q)
+    frame = channel.queue_declare(queue=q,durable=True,passive=True) 
+    message_count = frame.method.message_count
+    print("Queue "+q+" has \t"+str(message_count)+" messages")
+    if message_count>0:
+      channel.queue_purge(queue=q)
+      print("Purged queue "+q)
+  
+# channel.queue_declare(queue='temp-bkup',durable=True) # Declare a queue with name temp-bkup (not necessary as already declared 
+
+# create a function which is called on incoming messages
+# def callback(ch, method, properties, body):
+#   process_message(body)
+
+# set up subscription on the queue
+# for q in allQs:
+#     channel.basic_consume(q,  callback,  auto_ack=True)
+# channel.basic_consume("temp-bkup",  callback,  auto_ack=True)
+
+# start consuming (blocks)
+# channel.start_consuming()
+channel.close()
+connection.close()
+
