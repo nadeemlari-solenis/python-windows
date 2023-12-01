25e9cf1f125a909b405d82e1110f77b630217957
Nadeem <mnadeem@contractor.solenis.com>
2023-12-01 20:57:31 +0530

    updated

--- Web/sso.py
+++ Web/sso.py
@@ -17,15 +17,29 @@ elapsed_time = end - start
 print(f"Average Time: {elapsed_time*1000/count} ms")
 
 
-print("\nusing Azure CDN ")
+print("\nusing Azure CDN prd ")
 start = time.perf_counter()
 for i in tqdm(range(count)):
-    response = r.get('https://soleniscdn.azureedge.net/pages/AzureBlue/selfAsserted.html') 
-    response = r.get('https://soleniscdn.azureedge.net/pages/AzureBlue/unified.html')
-    response = r.get('https://soleniscdn.azureedge.net/pages/src/backgrounds/solenis_background.jfif') 
-    response = r.get('https://soleniscdn.azureedge.net/pages/src/css/solenis_styles.css')
-    response = r.get('https://soleniscdn.azureedge.net/pages/src/fonts/din_medium-webfont.woff')
-    response = r.get('https://soleniscdn.azureedge.net/pages/src/fonts/din_medium-webfont.woff2')
+    response = r.get('https://solenis-sso-cdn-prod.azureedge.net/pages/AzureBlue/selfAsserted.html') 
+    response = r.get('https://solenis-sso-cdn-prod.azureedge.net/pages/AzureBlue/unified.html')
+    response = r.get('https://solenis-sso-cdn-prod.azureedge.net/pages/src/backgrounds/solenis_background.jfif') 
+    response = r.get('https://solenis-sso-cdn-prod.azureedge.net/pages/src/css/solenis_styles.css')
+    response = r.get('https://solenis-sso-cdn-prod.azureedge.net/pages/src/fonts/din_medium-webfont.woff')
+    response = r.get('https://solenis-sso-cdn-prod.azureedge.net/pages/src/fonts/din_medium-webfont.woff2')
+    
+end = time.perf_counter()
+elapsed_time = end - start
+print(f"Average Time: {elapsed_time*1000/count} ms")
+
+print("\nusing Azure CDN test")
+start = time.perf_counter()
+for i in tqdm(range(count)):
+    response = r.get('https://soleni-sso-test-cdn.azureedge.net/pages/AzureBlue/selfAsserted.html') 
+    response = r.get('https://soleni-sso-test-cdn.azureedge.net/pages/AzureBlue/unified.html')
+    response = r.get('https://soleni-sso-test-cdn.azureedge.net/pages/src/backgrounds/solenis_background.jfif') 
+    response = r.get('https://soleni-sso-test-cdn.azureedge.net/pages/src/css/solenis_styles.css')
+    response = r.get('https://soleni-sso-test-cdn.azureedge.net/pages/src/fonts/din_medium-webfont.woff')
+    response = r.get('https://soleni-sso-test-cdn.azureedge.net/pages/src/fonts/din_medium-webfont.woff2')
     
 end = time.perf_counter()
 elapsed_time = end - start
